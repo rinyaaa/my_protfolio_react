e196072e01dbfe3e8409de1bd69d848aaf1b2218
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2024-06-04 18:00:18 +0900

    timelineつくったよ(pcモニターの場合)

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,31 +1,34 @@
 import React from 'react';
 
 function Timeline() {
-  return (
+    return (
+        <div className="timecolor">
+            <h2>History</h2>
 
-<section class="timeline">
-            <div class="container left">
-                <div class="content">
-                    <h2>2024年</h2>
-                    <p>最新の重要な出来事や達成を記述します。</p>
+            <section class="timeline">
+                <div class="container left">
+                    <div class="content">
+                        <h3>誕生日<p>2005年7月</p></h3>
+                        <p>長崎県で生まれる</p>
+                    </div>
                 </div>
-            </div>
-            <div class="container right">
-                <div class="content">
-                    <h2>2023年</h2>
-                    <p>2023年に起こった重要な出来事や達成を記述します。</p>
+                <div class="container right">
+                    <div class="content">
+                        <h3>高校入学<p>2021-2024</p></h3>
+                        <p>長崎県立諫早商業高等学校情報科</p>
+                    </div>
                 </div>
-            </div>
-            <div class="container left">
-                <div class="content">
-                    <h2>2022年</h2>
-                    <p>2022年に起こった重要な出来事や達成を記述します。</p>
+                <div class="container left">
+                    <div class="content">
+                        <h3>2022年</h3>
+                        <p>2022年に起こった重要な出来事や達成を記述します。</p>
+                    </div>
                 </div>
-            </div>
-           
-        </section>
 
-);
+            </section>
+            </div>
+
+            );
 }
 
-export default Timeline;
+            export default Timeline;
