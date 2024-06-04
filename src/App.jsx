e1366fd633c7297e6fbb6015fb1ae1b311ff6505
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2024-06-04 11:50:55 +0900

    timeline作ったけどづれまくり

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Header from './components/Header';
 import Profile from './components/Profile';
 import Skills from './components/Skills';
 import Licenses from './components/License';
+import Timeline from './components/Timeline';
 
 import './style.css';
 import './nav.css';
 import './profile.css';
 import './skills.css';
+import './timeline.css';
 
 function App() {
   return (
@@ -16,6 +18,7 @@ function App() {
       <Profile />
       <Skills />
       <Licenses />
+      <Timeline />
     </div>
   );
 }
---
+++ src/components/Timeline.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Timeline() {
+  return (
+
+<section class="timeline">
+            <div class="container left">
+                <div class="content">
+                    <h2>2024年</h2>
+                    <p>最新の重要な出来事や達成を記述します。</p>
+                </div>
+            </div>
+            <div class="container right">
+                <div class="content">
+                    <h2>2023年</h2>
+                    <p>2023年に起こった重要な出来事や達成を記述します。</p>
+                </div>
+            </div>
+            <div class="container left">
+                <div class="content">
+                    <h2>2022年</h2>
+                    <p>2022年に起こった重要な出来事や達成を記述します。</p>
+                </div>
+            </div>
+           
+        </section>
+
+);
+}
+
+export default Timeline;
