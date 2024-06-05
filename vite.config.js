876da44a341adafcc6ee5067810418c6ec7a5ab2
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2024-06-06 01:47:06 +0900

    vite.jsいじった

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,18 @@ export default defineConfig({
     host:true,
   },
   build: {
-    outDir: 'dist'  // これがデフォルトですが、確認しておきます
+    outDir: 'dist',  // これがデフォルトですが、確認しておきます
+    assetsInlineLimit: 0,
+    manifest: true,
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+        // JavaScriptファイルのMIMEタイプを設定
+        format: 'es', // ここで 'es' は "text/javascript" に相当します
+      },
+    },
   },
   plugins: [react()],
-})
+});
