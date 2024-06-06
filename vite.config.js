b956961393b846fa59c7d29d1858e280e93f303c
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2024-06-06 09:36:22 +0900

    vercel変えた

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base:'/my_protfolio_react',
+  plugins:[react()],
   server:{
     host:true,
   },
