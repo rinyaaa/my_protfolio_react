7f8ed60dc00c67be93947d5770ab4557b4b762da
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2024-06-06 20:13:17 +0900

    aa

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base:'/my_protfolio_react/',
-  plugins:[react()],
-  server:{
-    host:true,
-  },
+  base: '/my_protfolio_react/',
+  plugins: [react()],
   build: {
-    external: [
-      '/my_protfolio_react/assets/index-53lfyRL8.css' // 外部ファイルとして扱う
-    ],
-    outDir: 'dist',  // これがデフォルトですが、確認しておきます
-    assetsInlineLimit: 0,
-    manifest: true,
     rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]',
-        // JavaScriptファイルのMIMEタイプを設定
-        format: 'es', // ここで 'es' は "text/javascript" に相当します
-      },
-    },
-  },
+      external: [
+        '/my_protfolio_react/assets/index-53lfyRL8.css'
+      ]
+    }
+  }
 });
