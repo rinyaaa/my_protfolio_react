286d0167abbb3c128ceaf8e7855b78c530fdf820
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2024-06-06 20:07:19 +0900

    aa

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/my_protfolio_react',
+  base:'/my_protfolio_react/',
   plugins:[react()],
   server:{
     host:true,
