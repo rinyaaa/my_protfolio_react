0b21c515ad9b12336954e800f37094412a753b10
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2024-06-19 20:26:06 +0900

    少し修正

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App.jsx";
 import Load from "./components/Load.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Load />
-    <App />
   </React.StrictMode>
 );
