7a9ac4ae8490d2cb7aae3bc73b4f8ec3c0bbe1cd
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-02-09 22:37:27 +0900

    デザイン変更

--- src/components/License.jsx
+++ src/components/License.jsx
@@ -1,24 +1,23 @@
-import React from 'react';
+import React from "react";
 
 function Licenses() {
   return (
-    <section id="licenses">
-      <h2>License</h2>
-      <div className="lic_yok">
-        <ul className="lic year">
-          <li>取得年度</li>
-          <li>R3.9月26日</li>
-          <li>R4.9月25日</li>
-          <li>R4.12月27日</li>
-        </ul>
-        <ul className="lic mei">
-          <li>資格名</li>
-          <li>全商情報処理検定プログラミング部門２級</li>
-          <li>全商情報処理検定ビジネス情報部門１級</li>
-          <li>基本情報技術者試験</li>
-        </ul>
+    <section id="licenses" className="licenses-container">
+      <h2 className="license-title">Licenses</h2>
+      <div className="license-list">
+        <div className="license-item">
+          <p className="license-date">R3.9月26日</p>
+          <p className="license-name">全商情報処理検定プログラミング部門２級</p>
+        </div>
+        <div className="license-item">
+          <p className="license-date">R4.9月25日</p>
+          <p className="license-name">全商情報処理検定ビジネス情報部門１級</p>
+        </div>
+        <div className="license-item">
+          <p className="license-date">R4.12月27日</p>
+          <p className="license-name">基本情報技術者試験</p>
+        </div>
       </div>
-
     </section>
   );
 }
